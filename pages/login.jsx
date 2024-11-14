346de7b8d0fe8dd59ed1ec3ee0aa469920919241
Nandan Varma <74198723+nandan-varma@users.noreply.github.com>
2024-11-14 21:05:24 +0000

    login themes and more toaste notifs for online games

--- pages/login.jsx
+++ pages/login.jsx
@@ -35,7 +35,7 @@ export default function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
       <Card className="w-[350px]">
         <CardHeader>
           <CardTitle>Login</CardTitle>
